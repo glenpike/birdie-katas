1d8ca67583992ffd78795a8297882ccbddf5086c
Glen Pike <glen.pike@olioex.com>
2026-02-07 20:45:16 +0000

    Task 3 (amber) Handle the CaregiverAbsenceBookedEvent start time
    Check the effectiveFrom depending on the event, but we break the first
    test - because the futureDate is a year from now and not the end of our
    absence, we include the test visit and unassign incorrectly...
    We do however pass the test when the absence includes the visit...

--- src/availability-processor/typescript/src/handler.ts
+++ src/availability-processor/typescript/src/handler.ts
@@ -13,23 +13,21 @@ export class EventProcessor {
   async handleEvent(
     event: CaregiverPermanentUnavailabilityEvent | CaregiverAbsenceBookedEvent
   ): Promise<void> {
-    if (!isCaregiverPermanentUnavailabilityEvent(event)) {
-      return;
-    }
+    const effectiveFrom = isCaregiverPermanentUnavailabilityEvent(event) ? event.effectiveFrom : event.startTime;
 
     const futureDate = new Date(
-      event.effectiveFrom.getTime() + 365 * 24 * 60 * 60 * 1000
+      effectiveFrom.getTime() + 365 * 24 * 60 * 60 * 1000
     ); // 1 year in the future
 
     const visits = await this.visitRepo.getCalendar(
       event.caregiverId,
-      event.effectiveFrom,
+      effectiveFrom,
       futureDate
     );
 
     // Unassign all visits that occur after the permanent unavailability starts
     for (const visit of visits) {
-      if (visit.startTime >= event.effectiveFrom) {
+      if (visit.startTime >= effectiveFrom) {
         await this.visitRepo.unassign(visit.id, event.caregiverId);
       }
     }
