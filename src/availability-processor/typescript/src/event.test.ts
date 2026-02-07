16b2d2bc2f8e1590f82ca7b70eb2ea037bc89b81
Glen Pike <glen.pike@olioex.com>
2026-02-07 21:57:41 +0000

    Task 4: (red) Create test assertions for validating events
    We will assume a basic interface for common event attributes, so
    add tests / function for that.

---
+++ src/availability-processor/typescript/src/event.test.ts
@@ -0,0 +1,7 @@
+import { isValidCaregiverEvent } from "./events";
+
+describe("isValidCaregiverEvent", () => {
+  it("returns true if our event has correct attributes")
+  it("returns false if our event is missing attributes")
+  it("ignores extra attributes")
+})
